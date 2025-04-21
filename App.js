4cadc25ed2dc814cf739ed9c93c202f763001978
ljeure <ljeure@gmail.com>
2025-04-21 11:49:42 +0300

    Update App.js for testing day 2

--- App.js
+++ App.js
@@ -18,8 +18,8 @@ const WorldviewDaily = () => {
   React.useEffect(() => {
     // Get the day of the week (or use override for testing)
     // Uncomment the next line and set a number 1-7 to test a specific day
-    // const dayNumber = 2; // Test day 2 (Life Expectancy)
-    const dayNumber = getDayOfWeek();
+    const dayNumber = 2; // Test day 2 (Life Expectancy)
+    // const dayNumber = getDayOfWeek();
     setCurrentDay(dayNumber);
     setQuizLoaded(true);
   }, []);
